eece482ba214d672821cc3caa873e32d65bd3186
bajirao <bajirao@sphinx-solution.com>
2018-01-09 14:11:06 -0800

    Commit

--- app/src/main/java/com/pets/app/initialsetup/LoginActivity.kt
+++ app/src/main/java/com/pets/app/initialsetup/LoginActivity.kt
@@ -1,24 +1,14 @@
 package com.pets.app.initialsetup
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.pets.app.R
-
-import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
 }
