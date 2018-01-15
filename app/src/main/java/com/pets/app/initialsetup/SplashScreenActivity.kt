18a26d46d1ce11a9ee725aeb73eaded03e3118f6
bajirao <bajirao@sphinx-solution.com>
2018-01-15 10:03:27 -0800

    Commit

--- app/src/main/java/com/pets/app/initialsetup/SplashScreenActivity.kt
+++ app/src/main/java/com/pets/app/initialsetup/SplashScreenActivity.kt
@@ -4,35 +4,34 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import com.pets.app.R
-import com.pets.app.activities.adoption.AdoptionListActivity
 import com.pets.app.common.AppPreferenceManager
 
 class SplashScreenActivity : BaseActivity() {
 
-    private var mHandler: Handler? = null;
-    private val DELAY: Long = 3000;
+    private var mHandler: Handler? = null
+    private val DELAY: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        callHandler();
+        callHandler()
     }
 
     private fun callHandler() {
 
         mHandler = Handler()
         mHandler!!.postDelayed(Runnable {
-            //            val mIntent: Intent?
-//            if (!AppPreferenceManager.isSignIn()) {
-//                mIntent = Intent(this, LoginActivity::class.java);
-//            } else {
-//                mIntent = Intent(this, LandingActivity::class.java);
-//            }
-//            this.startActivity(mIntent)
-//            this.finish();
+            val mIntent: Intent?
+            if (!AppPreferenceManager.isSignIn()) {
+                mIntent = Intent(this, LoginActivity::class.java)
+            } else {
+                mIntent = Intent(this, LandingActivity::class.java)
+            }
+            this.startActivity(mIntent)
+            this.finish()
 
-            AdoptionListActivity.startActivity(this)
+//            AdoptionListActivity.startActivity(this)
 
         }, DELAY)
     }
