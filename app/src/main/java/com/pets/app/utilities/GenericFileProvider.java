a2fbc204b9fcb6613d0c3b80253d9ee632a1aa64
bajirao <bajirao@sphinx-solution.com>
2018-01-18 15:28:14 -0800

    Commit

---
+++ app/src/main/java/com/pets/app/utilities/GenericFileProvider.java
@@ -0,0 +1,9 @@
+package com.pets.app.utilities;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by BAJIRAO on 18 January 2018.
+ */
+public class GenericFileProvider extends FileProvider {
+}
