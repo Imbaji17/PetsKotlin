3088ffcb1c3abcc002a0ae0790ffebc872e19d63
bajirao <bajirao@sphinx-solution.com>
2018-01-22 14:03:17 -0800

    Commit

---
+++ app/src/main/java/com/pets/app/activities/MyPetsActivity.kt
@@ -0,0 +1,13 @@
+package com.pets.app.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.pets.app.R
+
+class MyPetsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_pets)
+    }
+}
