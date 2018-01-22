fd582d7aee85e4b99ad03339f6e1323617f0bd4d
bajirao <bajirao@sphinx-solution.com>
2018-01-22 14:08:54 -0800

    Commit

--- app/src/main/java/com/pets/app/activities/MyPetsActivity.kt
+++ app/src/main/java/com/pets/app/activities/MyPetsActivity.kt
@@ -2,12 +2,14 @@ package com.pets.app.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.widget.ViewFlipper
 import com.pets.app.R
 
 class MyPetsActivity : AppCompatActivity() {
 
     private var mViewFlipper: ViewFlipper? = null
+    private var mRecyclerView: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
