a0ee5c17e3a5d6b0fca41b2f5ae9a37a210e9a8b
Pratik Patil <pratik@sphinx-solution.com>
2018-01-25 18:49:53 +0530

    buy sell filter

--- app/src/main/java/com/pets/app/activities/ProductDetailActivity.kt
+++ app/src/main/java/com/pets/app/activities/ProductDetailActivity.kt
@@ -3,7 +3,6 @@ package com.pets.app.activities
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.RatingBar
@@ -69,9 +68,7 @@ class ProductDetailActivity : BaseActivity() {
 //                tvBuy.text = context.getString(R.string.sell)
 //            } else {
 //                tvBuy.text = context.getString(R.string.buy)
-//            }
+//            } `
         }
-
     }
-
 }
